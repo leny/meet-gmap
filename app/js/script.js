7378b93239a16cc180b0fd68b9a446ec1ca57461
leny <info@flatland.be>
2016-03-08 16:57:14 +0100

    Code at the end of the course

--- app/js/script.js
+++ app/js/script.js
@@ -12,36 +12,69 @@
 
     var $gmap,
         gMap,
-        fInitGMap;
+        fInitGMap,
+        fGetRandomPosition,
+        fAddMarker,
+        fAddZone;
 
     fInitGMap = function() {
         gMap = new google.maps.Map( $gmap[ 0 ], {
             "center": new google.maps.LatLng( 50.83, 4.35 ),
             "disableDefaultUI": true,
             "scrollwheel": false,
-            // see https://snazzymaps.com for ready-to-use styles
-            "styles": [
-                {
-                    "featureType": "water",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#FF00FF"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#FFFF00"
-                        }
-                    ]
-                }
-            ],
             "zoomControl": true,
-            "zoom": 14
+            "zoom": 7
+        } );
+    };
+
+    fGetRandomPosition = function( fNext ) {
+        $.ajax( {
+            "url": "./",
+            "method": "GET",
+            "dataType": "json",
+            "error": function( oXHR, sError ) {
+                console.error( sError );
+            },
+            "success": fNext
+        } );
+    };
+
+    fAddMarker = function( oEvent, fNext ) {
+        oEvent.preventDefault();
+
+        fGetRandomPosition( function( oPosition ) {
+            var gMarker;
+
+            gMarker = new google.maps.Marker( {
+                "map": gMap,
+                "position": new google.maps.LatLng( oPosition.latitude, oPosition.longitude )
+            } );
+
+            if ( fNext ) {
+                fNext( gMarker );
+            }
+        } );
+    };
+
+    fAddZone = function( oEvent ) {
+        fAddMarker( oEvent, function( gMarker ) {
+            var sZoneSize = window.prompt( "Quelle taille pour la zone (entre 1 et 50) ?" ),
+                iZoneSize = parseInt( sZoneSize );
+
+            if ( isNaN( iZoneSize ) || iZoneSize < 1 || iZoneSize > 50 ) {
+                iZoneSize = 25;
+            }
+
+            return new google.maps.Circle( {
+                "strokeColor": "#660033",
+                "strokeOpacity": 0.8,
+                "strokeWeight": 1,
+                "fillColor": "#660066",
+                "fillOpacity": 0.35,
+                "map": gMap,
+                "center": gMarker.getPosition(),
+                "radius": iZoneSize * 1000
+            } );
         } );
     };
 
@@ -50,6 +83,10 @@
         $gmap = $( "#gmap" );
 
         fInitGMap();
+
+        $( "#add-marker" ).on( "click", fAddMarker );
+
+        $( "#add-zone" ).on( "click", fAddZone );
 
     } );
 
